60d56939bc564f53df0b2b9246b93e4fecef9ff6
EVCOMPASSS <support@evcompass.in>
2026-09-18 12:02:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
@@ -12,7 +13,6 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    cloudflare(),
   ],
 })
-
-export default config
